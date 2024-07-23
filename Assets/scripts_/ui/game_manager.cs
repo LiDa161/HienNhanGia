c0d99b1e0301abdfffe7e35b907a8cc841b4e8ae
longph52829 <155860137+longph52829@users.noreply.github.com>
2024-07-23 12:37:58 +0700

    update_selection

---
+++ Assets/scripts_/ui/game_manager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_manager : MonoBehaviour
+{
+    public static game_manager instance;
+
+    public character[] characters;
+
+    public character current_character;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (characters.Length > 0)
+        {
+            current_character = characters[0];
+        }
+    }
+
+    public void SetCharacter(character character)
+    {
+        current_character = character;
+    }
+
+
+}
