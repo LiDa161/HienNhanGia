728d4a16be626967f7ddeeb8bd3dbb4c7ec5e58e
longph52829 <155860137+longph52829@users.noreply.github.com>
2024-07-25 09:12:25 +0700

    update_selection3

--- Assets/scripts_/health/health_quai.cs
+++ Assets/scripts_/health/health_quai.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 public class health_quai : MonoBehaviour
 {
-
-    [SerializeField] TextMeshPro value_text;
-    [SerializeField] GameObject damage_text;
     public float max_health;
     public float current_health;
 
@@ -20,9 +17,6 @@ public class health_quai : MonoBehaviour
     public void tru_mau(float tru_mau)
     {
         current_health -= tru_mau;
-        //GameObject dam_text = Instantiate(damage_text, transform.position, Quaternion.identity);
-        value_text.text = tru_mau.ToString();
-        //Destroy(dam_text, 1);
         Debug.Log($"da tru : {tru_mau}");
         Debug.Log($"luong hp con lai : {current_health}");
     }
