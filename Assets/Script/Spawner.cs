49741ad892b9a06474c84b89a0b2f2a1316b32b1
Da <duyanhnehi@gmail.com>
2024-08-01 11:26:12 +0700

    UpdateSpawn

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -6,6 +6,7 @@ using static Cinemachine.DocumentationSortingAttribute;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
     float timer;
 
@@ -30,4 +31,13 @@ public class Spawner : MonoBehaviour
         GameObject enemy = GameManager.Instance.pool.Get(Random.Range(0, 2));
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
