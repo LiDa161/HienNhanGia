663ff63217dcabb0f1bb7e9fbdfe650e1354fc03
phamquanghuy091 <phamquanghuy16052005@gmail.com>
2024-08-03 00:11:42 +0700

    bangdiem

---
+++ Assets/Scripts/GetHighScore.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetHighScore : MonoBehaviour
+{
+    
+    public TMP_InputField token;
+    public TextMeshProUGUI thongbao;
+
+
+    public void GetScorebotton()
+    {
+        StartCoroutine(GetScore());
+    }
+    IEnumerator GetScore()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("token", token.text);
+        UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/GetHighscore.php",form);
+        yield return www.SendWebRequest();
+
+
+        if (!www.isDone)
+        {
+            thongbao.text = "Bạn cần đăng nhập để thực hiện thao tác này: tiến hành đăng nhập lại.";
+            Debug.Log("Kết nối không thành công: " + www.error);
+        }
+        else
+        {
+            string get = www.downloadHandler.text;
+            if (get.Contains("Lỗi"))
+            {
+                thongbao.text = "Không kết nối được tới server";
+            }
+            else
+            {
+                thongbao.text = get;
+            }
+
+        }
+
+
+    }
+
+    
+}
---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HighScore : MonoBehaviour
+{
+    public TMP_InputField diem;
+    public TMP_InputField token;
+    public TMP_InputField playername;
+    public TextMeshProUGUI thongbao;
+
+    public void Hightscorebotton()
+    {
+        StartCoroutine(InsertScore());
+    }
+
+    IEnumerator InsertScore()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("playerName", playername.text);
+        form.AddField("token", token.text);
+        form.AddField("score", diem.text);
+        UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/InsertHighscore.php", form);
+        yield return www.SendWebRequest();
+
+        if (!www.isDone)
+        {
+            thongbao.text = "Bạn cần đăng nhập để thực hiện thao tác này: tiến hành đăng nhập lại.";
+        }
+        else
+        {
+            string get = www.downloadHandler.text;
+            if (get == "Done")
+            {
+                thongbao.text = "Đã lưu dữ liệu lên server thành công";
+            }
+            else
+            {
+                thongbao.text = "Không kết nối được server";
+            }
+             
+        }
+    }
+}
