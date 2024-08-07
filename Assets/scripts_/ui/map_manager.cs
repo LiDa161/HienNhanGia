b54eadc1e15f35ef4169a22798d7e9f5ecea3af7
longph52829 <155860137+longph52829@users.noreply.github.com>
2024-08-08 00:20:55 +0700

    update_new_scene

---
+++ Assets/scripts_/ui/map_manager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class map_manager : MonoBehaviour
+{
+    public Button button_lv;    
+}
--- Assets/scripts_/ui/scene_manager.cs
+++ Assets/scripts_/ui/scene_manager.cs
@@ -1,12 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class scene_manager : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadScene(string scene_name)
     {
-        SceneManager.LoadSceneAsync(sceneName);
+        SceneManager.LoadSceneAsync(scene_name);
+    }
+
+    public void open_map(int index)
+    {
+        var name = $"map_{index}";
+        SceneManager.LoadScene(name);
+    }
+
+    public void unlock_new_map()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("unlock"))
+        {
+            PlayerPrefs.SetInt("unlock", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("unlocked_map", PlayerPrefs.GetInt("unlocked_map", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void rest_map()
+    {
+        PlayerPrefs.DeleteAll();
     }
 }
